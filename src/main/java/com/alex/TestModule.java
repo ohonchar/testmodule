2f3fdd109a50a82f3f32693921bc571dcc9b029d
ohonchar <ohonc@sofserveinc.com>
2020-06-18 18:09:41 +0300

    iniit2

--- src/main/java/com/alex/TestModule.java
+++ src/main/java/com/alex/TestModule.java
@@ -1,7 +1,11 @@
 package com.alex;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TestModule {
     public void getPrint(){
+        log.info("asdfasd");
         System.out.println("Hello world Alex Honchar");
     }
 }
