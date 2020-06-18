a15b609b1b8585d39c15127562c68eb1ea3ff344
ohonchar <ohonc@sofserveinc.com>
2020-06-18 17:31:30 +0300

    iniit

--- src/main/java/com/alex/TestModule.java
+++ src/main/java/com/alex/TestModule.java
@@ -1,10 +1,7 @@
 package com.alex;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class TestModule {
     public void getPriinty(){
-        log.info("Hello");
+        System.out.println("Hello");
     }
 }
